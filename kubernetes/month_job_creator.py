87b9b95ef72c33b79424b0be3ad927f5f6983e4d
Armin Bernstetter <armin.bernstetter@stud-mail.uni-wuerzburg.de>
2021-04-29 12:20:51 +0200

    ignore all jobs subfolders. these only bloat the repo. can easily be generated again from template

--- kubernetes/month_job_creator.py
+++ kubernetes/month_job_creator.py
@@ -4,11 +4,11 @@ import jinja2
 if __name__ == '__main__':
     templateLoader = jinja2.FileSystemLoader(searchpath="./")
     templateEnv = jinja2.Environment(loader=templateLoader)
-    TEMPLATE_FILE = "templates/corpusfiles_template.yaml"
+    TEMPLATE_FILE = "analysis/counts_template.yaml"
 
     template = templateEnv.get_template(TEMPLATE_FILE)
 
-    dir = Path("prep/jobs/")
+    dir = Path("analysis/jobs/")
     dir.mkdir(exist_ok=True)
 
     for timestep in [201905, 201906, 201907, 201908, 201909, 201910, 201911, 201912, 202001, 202002, 202003, 202004]:
