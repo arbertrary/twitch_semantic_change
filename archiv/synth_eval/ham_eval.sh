a2f90b191f179ef9f1c48c5691d7646fc0725df8
Armin Bernstetter <armin.bernstetter@stud-mail.uni-wuerzburg.de>
2021-05-08 17:53:10 +0200

    evaluation scripts + added a lot of eval results to archiv to be referenced in thesis

---
+++ archiv/synth_eval/ham_eval.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT=/home/stud/bernstetter/ma/mainrepo/evaluation/evaluate_synthetic_data_results.py
+DIR=/home/stud/bernstetter/ma/mainrepo/archiv/synth_eval/hamilton/
+TW_PSEUDO=/home/stud/bernstetter/datasets/synthetic_twitch/pseudoword_dict.json
+DTA_PSEUDO=/home/stud/bernstetter/datasets/dta/synth_dta/pseudoword_dict.json
+
+for file in $DIR/tw*.tsv
+do
+	python $SCRIPT --results_dir=$DIR --results_fn=$(basename $file) --pseudoword_design_dict=$TW_PSEUDO --word_column=1
+done
+
+for file in $DIR/dta*.tsv
+do
+	python $SCRIPT --results_dir=$DIR --results_fn=$(basename $file) --pseudoword_design_dict=$DTA_PSEUDO --word_column=1
+done
--- evaluation/get_ap_at_50.py
+++ evaluation/get_ap_at_50.py
@@ -2,16 +2,28 @@ import argparse
 import os
 import csv
 
+
+def read_ap_at_50(filepath):
+    with open(filepath, "r") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter="\t")
+        for row in reader:
+            if row["k_retrieved"] == "50":
+                print(row["average_precision"])
+
+
 if __name__ == '__main__':
-        parser = argparse.ArgumentParser(description="read one cell")
-        parser.add_argument("-in", "--in_file")
+    parser = argparse.ArgumentParser(description="read one cell")
+    parser.add_argument("-in", "--in_file")
 
-        args = parser.parse_args()
-        filepath = args.in_file
+    args = parser.parse_args()
+    in_path = args.in_file
 
-        with open(filepath, "r") as csvfile:
-            reader = csv.DictReader(csvfile, delimiter="\t")
-            for row in reader:
-                if row["k_retrieved"] == "50":
-                    print(row["average_precision"])
+    if os.path.isfile(in_path):
+        read_ap_at_50(in_path)
+    elif os.path.isdir(in_path):
+        for file in sorted(os.listdir(in_path)):
+            print("\n")
+            print("# " +file)
+            in_file = os.path.join(in_path, file)
+            read_ap_at_50(in_file)
 
