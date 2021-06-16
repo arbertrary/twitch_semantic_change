ca5c5142a5c8d3c0bb5efdacfb668e9bbf1c9718
Armin Bernstetter <armin.bernstetter@stud-mail.uni-wuerzburg.de>
2021-06-16 20:06:45 +0200

    bulk commit for all the kubernetes yaml configs of the last weeks

--- kubernetes/month_job_creator.py
+++ kubernetes/month_job_creator.py
@@ -4,11 +4,11 @@ import jinja2
 if __name__ == '__main__':
     templateLoader = jinja2.FileSystemLoader(searchpath="./")
     templateEnv = jinja2.Environment(loader=templateLoader)
-    TEMPLATE_FILE = "analysis/counts_template.yaml"
+    TEMPLATE_FILE = "templates/twitch_prep_template.yaml"
 
     template = templateEnv.get_template(TEMPLATE_FILE)
 
-    dir = Path("analysis/jobs/")
+    dir = Path("prep/fix_emotes/")
     dir.mkdir(exist_ok=True)
 
     for timestep in [201905, 201906, 201907, 201908, 201909, 201910, 201911, 201912, 202001, 202002, 202003, 202004]:
