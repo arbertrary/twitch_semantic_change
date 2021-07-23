2ec09840ad10973acc5cfc475063ede7d872f90c
Armin Bernstetter <armin.bernstetter@stud-mail.uni-wuerzburg.de>
2021-07-23 15:50:56 +0200

    mm vocab building with dir as input (instead of file)

--- multimodal/vocab.py
+++ multimodal/vocab.py
@@ -9,23 +9,32 @@ import json
 
 def build_global_vocabulary(in_path: str, out_path: str, min_count: int, skip_emotes: int):
     vocab = {}
-    filepath = in_path
-    with open(filepath, "r", encoding="utf-8") as csvfile:
-        reader = DictReader(csvfile, delimiter="\t")
-        for row in reader:
-            message = row["msg"].strip().split()
-            emotenames = row["emotenames"].split()
 
-            for word in message:
-                if skip_emotes == 1 and word in emotenames:
-                    continue
-                if word not in vocab:
-                    vocab[word] = {"emotes": Counter(emotenames), "count": 1}
-                else:
-                    vocab[word]["emotes"].update(emotenames)
-                    vocab[word]["count"] += 1
+    if os.path.isfile(in_path):
+        filepaths = [in_path]
+    elif os.path.isdir(in_path):
+        filepaths = [os.path.join(in_path, d) for d in os.listdir(in_path)]
+    else:
+        raise ValueError
 
-    c = {x: {"emotes": {k:dct["emotes"][k] for k in dct["emotes"] if dct["emotes"][k] >= min_count}, "count": dct["count"]} for x, dct in
+    for file in filepaths:
+        with open(file, "r", encoding="utf-8") as tsvfile:
+            reader = DictReader(tsvfile, delimiter="\t")
+            for row in reader:
+                message = row["msg"].strip().split()
+                emotenames = row["emotenames"].split()
+
+                for word in message:
+                    if skip_emotes == 1 and word in emotenames:
+                        continue
+                    if word not in vocab:
+                        vocab[word] = {"emotes": Counter(emotenames), "count": 1}
+                    else:
+                        vocab[word]["emotes"].update(emotenames)
+                        vocab[word]["count"] += 1
+
+    c = {x: {"emotes": {k: dct["emotes"][k] for k in dct["emotes"] if dct["emotes"][k] >= min_count},
+             "count": dct["count"]} for x, dct in
          sorted(vocab.items(), key=lambda y: y[1]["count"], reverse=True) if
          dct["count"] >= min_count}
 
@@ -35,25 +44,29 @@ def build_global_vocabulary(in_path: str, out_path: str, min_count: int, skip_em
 
 def build_local_vocabulary(in_path: str, out_path: str, min_count: int, skip_emotes: int):
     counter = Counter()
+    if os.path.isfile(in_path):
+        filepaths = [in_path]
+    elif os.path.isdir(in_path):
+        filepaths = [os.path.join(in_path, d) for d in os.listdir(in_path)]
+    else:
+        raise ValueError
 
-    # for file in os.listdir(in_path):
-    #    filepath = os.path.join(in_path, file)
-    filepath = in_path
-    with open(filepath, "r", encoding="utf-8") as csvfile:
-        reader = DictReader(csvfile, delimiter="\t")
-        for row in reader:
-            message = row["msg"].strip().split()
-            emotenames = row["emotenames"].split()
+    for file in filepaths:
+        with open(file, "r", encoding="utf-8") as tsvfile:
+            reader = DictReader(tsvfile, delimiter="\t")
+            for row in reader:
+                message = row["msg"].strip().split()
+                emotenames = row["emotenames"].split()
 
-            tuplelist = []
-            for word in message:
-                if skip_emotes == 1 and word in emotenames:
-                    continue
-                tpl = "|".join([word] + emotenames)
-                tuplelist.append(tpl)
+                tuplelist = []
+                for word in message:
+                    if skip_emotes == 1 and word in emotenames:
+                        continue
+                    tpl = "|".join([word] + emotenames)
+                    tuplelist.append(tpl)
 
-            print(tuplelist)
-            counter.update(tuplelist)
+                print(tuplelist)
+                counter.update(tuplelist)
 
     c = {x: count for x, count in sorted(counter.items(), key=lambda y: y[1], reverse=True) if count >= min_count}
 
@@ -73,11 +86,11 @@ if __name__ == '__main__':
     indir = args.infiles_rootdir
     outdir = args.outdir_path
     os.makedirs(os.path.dirname(outdir), exist_ok=True)
-    
+
     if args.local_vocab:
         build_local_vocabulary(indir, outdir, args.min, args.skip_emotes)
     else:
         build_global_vocabulary(indir, outdir, args.min, args.skip_emotes)
-        
-    #build_vocabulary2("../data/testdata/emote_filtered/filtered_201911031555.txt", "vocab.json", min_count=2,
+
+    # build_vocabulary2("../data/testdata/emote_filtered/filtered_201911031555.txt", "vocab.json", min_count=2,
     #                   skip_emotes=1)
